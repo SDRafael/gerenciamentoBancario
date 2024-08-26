94bb8db429079eaf8035596d0e510f2ff375f2dd
Rafael Souza <67163593+SDRafael@users.noreply.github.com>
2024-08-26 20:07:07 -0300

    Adicionando funcionalidade para o gerente

--- gerenciar conta/Conta.py
+++ gerenciar conta/Conta.py
@@ -1,50 +1,90 @@
+from datetime import datetime;
+
 class Conta:
-    def __init__(self, nome, nConta, tipo):
+    def __init__(self, nome, cpf, nConta, dataAbertura, tipo):
         self.nome = nome
-        #self.data = data
+        self.dataAbertura = dataAbertura
         self.tipo = tipo
-        self.saldo = 0.
+        self.saldo = 0
         self.histMovimentacoes = []
-        self.__nConta = nConta
+        self.nConta = nConta
+        self.cpf = cpf
 
-    def deposito(self, valores):
-        self.saldo += valores
-        
-        self.histMovimentacoes.append(f"Deposito de R$ {valores:.2f}")
-
-    def saque(self, valores):
+    def detalhes(self):
+        print(f"""
+                Nome: {self.nome}
+                cpf: {self.cpf}
+                Numero da conta:{self.nConta}
+                Data da abertura:{self.dataAbertura}
+                TIpo de conta: {self.tipo}
+              """)
+    def depositar(self, valores):
+        if valores > 0:
+            self.saldo += valores
+            
+            self.histMovimentacoes.append(f"Deposito de R$ {valores:.2f} em {datetime.now()}")
+            print(f"Valor depositado de R$ {valores}. Saldo atual de R${self.saldo}")
+        else:
+            print(f"Depósito inválido")
+            
+    def sacar(self, valores):
         if valores <= self.saldo:
             self.saldo -= valores
-            self.histMovimentacoes.append(f"Saque de R$ {valores:.2f}")
+            self.histMovimentacoes.append(f"Saque de R$ {valores:.2f} em {datetime.now()}")
+            print(f"Saque efetuado no valor de R${valores}")
         else:
             print(f"Operação negada para SAQUE {valores:.2f} por saldo insuficiente.")
-            self.histMovimentacoes.append(f"Operação negada para SAQUE {valores:.2f} por saldo insuficiente.")
+            self.histMovimentacoes.append(f"Operação negada para SAQUE {valores:.2f} em {datetime.now()} por saldo insuficiente.")
 
-    def extrato(self):
-        extrato = []
-        consulta = f"Extrato bancário - conta: {self.__nConta}, {self.nome} - saldo: {self.saldo}"
-        extrato.append(consulta)
-        return extrato
+    def getSaldo(self):
+        return self.saldo
+    def verExtrato(self):
+        print(f"Extrato bancário - conta: {self.nConta}, {self.nome} - saldo: {self.saldo}")
+        for mov in self.histMovimentacoes:
+            print(mov)
+
+        print(f"saldo atual de R${self.saldo}")
+        
     
 
 class Sistema:
-    def __init__(self) -> None:
-        self.listaContas = []
+    def __init__(self):
+        #self.listaContas = []
         self.contas = {
 
         }    
 
-    def __cadastrar(self, nome, cpf):
-        self.contas["nome"] = nome
-        self.contas["cpf"] = cpf
-        self.contas["numero"] = None
-        return
-    
-    def cadastro(self, nome, cpf):
-        if cpf in self.contas:
-            return "CPF já cadastrado"
-        else:
-            self.__cadastrar(nome, cpf)
-
+    def cadastrar(self, nome, cpf, nConta, dataAbertura, tipo):
+        cadastro = Conta(nome, cpf, nConta, dataAbertura, tipo)
         
-    
+        if nConta in self.contas:
+            print(f"A conta de número {nCOnta} já existe. Operação NEGADA!")
+
+        else:
+            self.contas[nConta] = cadastro
+            print(f"Cadastro efetuado para conta de número {nConta}!")
+
+    def consultaConta(self, nConta):
+        return self.contas.get(nConta, None)
+        
+gerente = Sistema()
+
+gerente.cadastrar("Maria do Ibura", "000.000.000-01", "001", "2024-02-01", "corrente" )
+gerente.cadastrar("Tõe de Irineu do Peixe", "100.001.000-02", "002", "2024-04-02", "poupança")
+gerente.cadastrar("Jessé da Ambulância", "098.000.020-20", "003", "2024-08-06", "corrente")
+
+
+consultaGerente = gerente.consultaConta("001")
+"""if consultaGerente:
+    consultaGerente.detalhes()
+
+else:
+    print("Conta inexistente")"""
+#
+# ABAIXO UM PEQUENO TESTE
+#
+print(consultaGerente.getSaldo())
+consultaGerente.depositar(500)
+print(f"pós deposito {consultaGerente.getSaldo()}")
+consultaGerente.sacar(300)
+consultaGerente.verExtrato()
